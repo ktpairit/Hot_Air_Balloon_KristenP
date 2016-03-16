58ce183980f613fe047227944fbbff0388c3abf8
Kristen Pairitz <ktpairit@gmail.com>
2016-03-16 19:49:59 -0400

    add styling and modify header menu

--- header.php
+++ header.php
@@ -10,27 +10,23 @@
 <body>
 
     <div class="container">
+        <!--Beginning of Header-->
         <header class="row">
-            <div class="twelve columns">
-                    <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
-                    <p><?php bloginfo('description'); ?></p>
-            </div>
-
-            <!--------- Add Search Form
-            <div class="three columns">
-                <?php get_search_form(); ?>
-            </div>
-            End Search Form -------->
-
-        </header>
-        <!------ END OF HEADER - BEGIN SECTION CONTENT ----->
-
-        <!-- Add Menu Here -->
-        <div class="row">
+            <!-- Add Menu Here -->
             <div class="twelve columns">
                 <?php wp_nav_menu(array(
+                    'menu' => 'Main Menu',
                     'sort_column' => 'menu_order',
                     'container_class' => 'blank-menu-header'
-                    ));?>
+                ));?>
             </div>
-        </div>
+            <!--End of Menu-->
+           <div class="row">
+                <div class="twelve columns">
+                    <h1><a href="<?php bloginfo('url'); ?>"><?php bloginfo('name'); ?></a></h1>
+                    <p><?php bloginfo('description'); ?></p>
+                </div>
+            </div>
+        </header>
+        <!-- End of Header - Begin Section Content-->
+
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
         <section class="row">
             <div class="nine columns">
-        <!-- BEGIN LOOP -->
+        <!--Begin Loop-->
                 <?php
                 if ( have_posts() ) {
                     while ( have_posts() ) {
@@ -13,16 +13,16 @@
                     <?php } // end while
                 } // end if
                 ?>
-        <!-- END LOOP -->
+        <!--End Loop-->
             </div>
             <div class="three columns">
-            <!-- BEGIN SIDEBAR -->
-            <!-- Add Search Form -->
+            <!--Begin Sidebar-->
+            <!--Add Search Form-->
                 <?php get_search_form(); ?>
-                <!-- End Search Form -->
+                <!--End Search Form-->
                 <?php get_sidebar(); ?>
                 <p>I'M RIGHT HERE AND I"M A PART OF A PAGE!!!</p>
-            <!-- END SIDEBAR -->
+            <!--End Sidebar-->
             </div>
         </section>
 
