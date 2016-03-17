82f1e7870f5e7d2f1b0d7d44da2670710ae160df
Kristen Pairitz <ktpairit@gmail.com>
2016-03-16 22:19:09 -0400

    add structure and styling to header and search/archive pages

--- archive.php
+++ archive.php
@@ -3,28 +3,33 @@
 
 get_header(); ?>
 
+<!--Beginning of Archive Page-->
 <section class="row">
-    <div class="twelve columns">
+    <div class="nine columns">
         <?php if ( have_posts() ) : ?>
             <h2>Archives</h2>
+            <p>A Page for our Archives</p>
             <?php
             // The Loop
             while ( have_posts() ) : the_post();?>
-            <!-- data context -->
+            <!--Data Context-->
                 <h2>
                     <a href="<?php the_permalink() ?>">
                         <?php the_title(); ?>
                     </a>
                 </h2>
-                <?php the_excerpt(); ?>
-            <?php endwhile; ?> <!-- End Loop -->
+            <?php the_excerpt(); ?>
+            <?php endwhile; ?>
+            <!-- End Loop -->
             <?php else: ?>
             <p>Sorry, no posts matched your criteria.</p>
         <?php endif; ?>
     </div>
-</section>
-<section class="row">
-    <div class="twelve columns">
+    <div class="three columns">
+        <!--Begin Sidebar-->
+        <!--Add Search Form-->
+            <?php get_search_form(); ?>
+        <!--End Search Form-->
         <h2>Archives by Month:</h2>
         <ul>
             <?php wp_get_archives('type=monthly'); ?>
@@ -33,7 +38,28 @@ get_header(); ?>
         <ul>
              <?php wp_list_categories(); ?>
         </ul>
+        <!--End Sidebar-->
+        </div>
+
+</section>
+<!--End of Archive Page-->
+
+<!--
+<section class="row">
+    <div class="six columns">
+        <h2>Archives by Month:</h2>
+        <ul>
+            <?php wp_get_archives('type=monthly'); ?>
+        </ul>
+    </div>
+    <div class="six columns">
+        <h2>Archives by Category:</h2>
+        <ul>
+             <?php wp_list_categories(); ?>
+        </ul>
     </div>
 </section>
+-->
+
 
 <?php get_footer(); ?>
